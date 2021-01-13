793fce8285d049cf3a939f9fcea88a13dca6b474
haybuka <paschal.nduleo@gmail.com>
2021-01-13 06:24:18 -0800

    Adjusted navbar and nav toggle,with color transition effects

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,8 @@
 let harmburger = document.querySelector('.harmburger')
 let navItem = document.querySelector('.nav--item')
+let main = document.querySelector('.overlay')
 
 harmburger.addEventListener('click',()=>{
  navItem.classList.toggle('open')
+ main.classList.toggle('over')
 })
