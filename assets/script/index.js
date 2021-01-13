aa3a179cd3f3f9667f7b444371419c375e03b94f
haybuka <paschal.nduleo@gmail.com>
2021-01-13 12:23:16 -0800

    banner section 80% complete

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,8 @@
 let harmburger = document.querySelector('.harmburger')
 let navItem = document.querySelector('.nav--item')
-let main = document.querySelector('.overlay')
+let overlay = document.querySelector('.overlay')
 
 harmburger.addEventListener('click',()=>{
  navItem.classList.toggle('open')
- main.classList.toggle('over')
+ overlay.classList.toggle('over')
 })
