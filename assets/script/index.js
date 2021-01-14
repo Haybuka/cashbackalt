549641199b5c09802cfc634f0da94485f8149697
haybuka <paschal.nduleo@gmail.com>
2021-01-14 17:49:21 +0100

    Created js digital clock and also adjusted most icon

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,56 @@
 let harmburger = document.querySelector('.harmburger')
 let navItem = document.querySelector('.nav--item')
 let overlay = document.querySelector('.overlay')
+let hour = document.querySelector('.time--hour')
+let minute = document.querySelector('.time--minute')
+let seconds = document.querySelector('.time--seconds')
 
 harmburger.addEventListener('click',()=>{
  navItem.classList.toggle('open')
  overlay.classList.toggle('over')
 })
+setInterval(() => {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    hour.innerHTML = h;
+    minute.innerHTML = m;
+    seconds.innerHTML = s;
+if(h <10){
+    hour.innerHTML = `0${h}`;
+}
+if(m <10){
+    minute.innerHTML = `0${m}`;
+}
+console.log(`${h}:${m}:${s}`)
+}, 1000);
+// function showTime(){
+//     let date = new Date();
+//     let h = date.getHours(); // 0 - 23
+//     let m = date.getMinutes(); // 0 - 59
+//     let s = date.getSeconds(); // 0 - 59
+//     let session = "AM";
+    
+//     if(h == 0){
+//         h = 12;
+//     }
+    
+//     if(h > 12){
+//         h = h - 12;
+//         session = "PM";
+//     }
+    
+//     h = (h < 10) ? "0" + h : h;
+//     m = (m < 10) ? "0" + m : m;
+//     s = (s < 10) ? "0" + s : s;
+    
+//     let time = h + ":" + m + ":" + s + " " + session;
+//     document.getElementById("MyClockDisplay").innerText = time;
+//     document.getElementById("MyClockDisplay").textContent = time;
+    
+//     setTimeout(showTime, 1000);
+    
+// }
+
+// showTime();
